832b6b42aa8a3ec427f6549dd5640138669a30c2
Sirichai <6410110503@psu.ac.th>
2024-09-29 17:56:13 +0700

    update3

--- config_port.py
+++ config_port.py
@@ -25,7 +25,7 @@ try:
             f'ip dhcp pool {vlan_list[vlan]}',
             f'network 192.168.{vlan_list[vlan]}.0 255.255.255.0'
             f'default-router 192.168.{vlan_list[vlan]}.1'
-            'dns-server 8.88.8'
+            'dns-server 8.88.8',
             f'int {port}',
             'switchport mode access',
             f'switchport access vlan {vlan_list[vlan]}'
